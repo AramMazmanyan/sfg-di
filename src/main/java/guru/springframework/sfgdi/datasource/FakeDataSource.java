98169d546fab2f5a79553012b09515fd92043253
aram.mazmanyan <mazmanyanaram1995@gmail.com>
2022-01-14 19:36:02 +0400

    AppPropertyTesting

---
+++ src/main/java/guru/springframework/sfgdi/datasource/FakeDataSource.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgdi.datasource;
+
+public class FakeDataSource {
+
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getJdbcUrl() {
+        return jdbcUrl;
+    }
+
+    public void setJdbcUrl(String jdbcUrl) {
+        this.jdbcUrl = jdbcUrl;
+    }
+
+    private String jdbcUrl;
+
+}
--- src/main/java/guru/springframework/sfgdi9/services/GreetingService.java
+++
@@ -1,5 +0,0 @@
-package guru.springframework.sfgdi9.services;
-
-public interface GreetingService {
-    String sayGreeting();
-}
